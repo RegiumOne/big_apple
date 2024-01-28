e6aef8f1d07f2bd9d6ae8cc4d1f219659f6e6846
Oleksandr Hrubalskyi <oleksandr.hrubalskyi@icloud.com>
2024-01-28 15:17:29 +0100

    fix: zone calculation

--- lib/main_world.dart
+++ lib/main_world.dart
@@ -5,26 +5,31 @@ import 'package:big_apple/components/zone_component.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame_tiled/flame_tiled.dart';
-// ignore: implementation_imports
-import 'package:flame_tiled/src/renderable_layers/tile_layers/tile_layer.dart';
 
 class MainWorld extends World {
   MainWorld({
-    required this.worldWidth,
-    required this.worldHeight,
     double tileWidth = 128,
     double tileHeight = 64,
   }) : tileSize = Vector2(tileWidth, tileHeight);
 
-  final double worldWidth;
-  final double worldHeight;
   final Vector2 tileSize;
+
+  double get worldWidth => _worldWidth;
+  double get worldHeight => _worldHeight;
+  int get columnCount => (_worldWidth / tileSize.x).floor();
+  int get rowCount => (_worldHeight / tileSize.y * 2).floor();
+
+  double _worldWidth = -1;
+  double _worldHeight = -1;
 
   late TiledComponent<FlameGame<World>> tiledMap;
 
   @override
   FutureOr<void> onLoad() async {
     tiledMap = await TiledComponent.load('map.tmx', tileSize);
+
+    _worldWidth = tiledMap.width;
+    _worldHeight = tiledMap.height;
 
     await add(tiledMap);
     await _addMill();
@@ -47,19 +52,33 @@ class MainWorld extends World {
   }
 
   Future<void> _initZones() async {
-    for (var layer in tiledMap.tileMap.renderableLayers) {
-      // ignore: invalid_use_of_internal_member
-      if (layer is FlameTileLayer && layer.layer.name == 'Background') {
-        final renderableLayer = layer;
-        for (var transform in renderableLayer.tiledAtlas.batch!.transforms) {
-          Vector2 position = Vector2(transform.tx, transform.ty);
-          Vector2 size = Vector2(tileSize.x / 2, tileSize.y);
-          ZoneComponent zoneComponent = ZoneComponent(tileSize: Vector2.all(tileSize.x))
-            ..position = position + Vector2(32, 0)
-            ..anchor = Anchor.topLeft
-            ..size = size;
-          add(zoneComponent);
-        }
+    TileLayer? backgroundLayer = tiledMap.tileMap.getLayer('Background');
+    if (backgroundLayer == null || backgroundLayer.data == null) return;
+
+    int rowCount = backgroundLayer.height;
+    int columnCount = backgroundLayer.width;
+
+    if (rowCount != backgroundLayer.height || columnCount != backgroundLayer.width) {
+      throw Exception('Map size does not match the number of tiles');
+    }
+
+    for (int row = 0; row < rowCount; row++) {
+      for (int column = 0; column < columnCount; column++) {
+        int tile = backgroundLayer.data![row * columnCount + column];
+        if (tile == 0) continue;
+
+        bool isEven = row % 2 == 0;
+
+        double x = column * tileSize.x + (isEven ? 0 : tileSize.x / 2);
+        double y = row * tileSize.y / 2;
+
+        Vector2 position = Vector2(x, y);
+        Vector2 size = Vector2(tileSize.x / 2, tileSize.y);
+        ZoneComponent zoneComponent = ZoneComponent(tileSize: Vector2.all(tileSize.x))
+          ..position = position + Vector2(32, 0)
+          ..anchor = Anchor.topLeft
+          ..size = size;
+        add(zoneComponent);
       }
     }
   }
