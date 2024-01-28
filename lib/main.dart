64ef99e7494cf200550ddf6f96238b9ea757b516
Oleksandr Hrubalskyi <oleksandr.hrubalskyi@icloud.com>
2024-01-28 16:25:23 +0100

    feat: setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:big_apple/app.dart';
+import 'package:big_apple/common/firebase/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await Flame.device.fullScreen();
   await Flame.device.setLandscape();
