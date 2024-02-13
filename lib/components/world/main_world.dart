e85bb2fbc06fe572cccbbe1a0d4ecdff9a7ddda0
Саша Стрельцов <sstreltsov55@gmail.com>
2024-02-13 20:24:30 +0200

    feat: Block stacking of buildings

--- lib/components/world/main_world.dart
+++ lib/components/world/main_world.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:big_apple/components/building_component.dart';
 import 'package:big_apple/components/zone_component.dart';
 import 'package:big_apple/data/models/building.dart';
+import 'package:collection/collection.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame_tiled/flame_tiled.dart';
@@ -24,6 +26,8 @@ class MainWorld extends World {
   double _worldHeight = -1;
 
   late TiledComponent<FlameGame<World>> tiledMap;
+
+  final List<Vector2> _initialBusyCoordinates = [];
 
   @override
   FutureOr<void> onLoad() async {
@@ -59,11 +63,24 @@ class MainWorld extends World {
 
         Vector2 position = Vector2(x, y);
         Vector2 size = Vector2(tileSize.x / 2, tileSize.y);
-        ZoneComponent zoneComponent = ZoneComponent(tileSize: Vector2.all(tileSize.x))
+
+        //it is important that _initialBusyCoordinates are initialized first in the initBuildings method, otherwise you need to check in initBuildings too if _initZones is initialized first
+        final checkPosition = _initialBusyCoordinates.firstWhereOrNull(
+          (element) => (element - Vector2(64, 0)) == position,
+        );
+
+        if (checkPosition != null) {
+          log('Busy position: $position');
+        }
+
+        ZoneComponent zoneComponent = ZoneComponent(
+          tileSize: Vector2.all(tileSize.x),
+          isAvailable: checkPosition == null,
+        )
           ..position = position + Vector2(32, 0)
           ..anchor = Anchor.topLeft
           ..size = size;
-        add(zoneComponent);
+        await add(zoneComponent);
       }
     }
   }
@@ -74,7 +91,10 @@ class MainWorld extends World {
         building: buidling,
         size: Vector2.all(tileSize.x),
       );
-      add(buildingComponent);
+
+      _initialBusyCoordinates.add(buildingComponent.position);
+
+      await add(buildingComponent);
     }
   }
 }
