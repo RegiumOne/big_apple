195eec99065982c9f388b07e61bdea2ce839106a
Aleksander Hrubalski <16018532+hrubalski@users.noreply.github.com>
2024-03-08 12:56:26 +0100

    fix: dependencies (#28)

--- lib/di/injector.dart
+++ lib/di/injector.dart
@@ -1,14 +1,14 @@
-import 'package:big_apple/di/injector.config.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+
+import 'package:big_apple/di/injector.config.dart';
 
 @InjectableInit(
   initializerName: r'$initGetIt',
   preferRelativeImports: true,
   asExtension: true,
 )
-Future<void> configureDependencies(String environment) async =>
-    await GetIt.instance.$initGetIt(environment: environment);
+Future<void> configureDependencies(String environment) => GetIt.instance.$initGetIt(environment: environment);
 
 T inject<T extends Object>({
   String? instanceName,
