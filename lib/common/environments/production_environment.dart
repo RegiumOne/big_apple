67709bf1ffd61e554d9fce99c3153e296acf2e16
Oleksandr Hrubalskyi <oleksandr.hrubalskyi@icloud.com>
2024-03-08 12:35:23 +0100

    fix: dependencies

--- lib/common/environments/production_environment.dart
+++ lib/common/environments/production_environment.dart
@@ -1,8 +1,9 @@
-import 'package:big_apple/common/environments/app_environment.dart';
 import 'package:injectable/injectable.dart';
 
-@LazySingleton(as: AppEnvironment)
+import 'package:big_apple/common/environments/app_environment.dart';
+
 @prod
+@LazySingleton(as: AppEnvironment)
 class ProductionEnvironment implements AppEnvironment {
   @override
   String host = "https://cloud.appwrite.io/v1";
